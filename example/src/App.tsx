947c7a989344af55e213de68eb4c68f650405c8f
sox-dev <soxuser1@gmail.com>
2023-01-18 05:53:19 +0900

    update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,33 +5,50 @@ import { registViewModel } from "property-process";
 type CountType = {
   count: number;
   multiply: number;
+  nested: {
+    test: string[];
+    deepTest: {
+      level: number;
+    };
+  };
   increase: () => void;
   decrease: () => void;
 };
 
-const { watcher, handler } = registViewModel<CountType>({
-  count: 0,
-  multiply: 0,
-  increase() {
-    handler.state.count += 1;
+const { watcher, handler } = registViewModel<CountType>(
+  {
+    count: 0,
+    multiply: 0,
+    nested: {
+      test: [],
+      deepTest: {
+        level: 1,
+      },
+    },
+    increase() {
+      handler.state.count += 1;
+    },
+    decrease() {
+      handler.state.count -= 1;
+    },
   },
-  decrease() {
-    handler.state.count -= 1;
-  },
-});
+  { deep: true }
+);
 
 handler.on("count", () => {
   handler.state.multiply = handler.state.count * 2;
 });
 
 function App() {
-  const state = watcher(["count"]);
+  const state = watcher(["count", "nested"]);
+  console.log(state);
   return (
     <div
       style={{
         width: "100%",
         height: "100%",
         display: "flex",
+        flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
       }}
@@ -41,6 +58,7 @@ function App() {
         <span>{state.count}</span>
         <button onClick={state.decrease}>-</button>
       </div>
+      <button onClick={() => state.nested.test.push("1")}>Nested Test</button>
     </div>
   );
 }
